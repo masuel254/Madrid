8a0ca4700e733b28cadd5b1235cd5d564c356408
masuel254 <samuel.morieux@gmail.com>
2026-07-31 14:02:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //  L'ancien cache est automatiquement supprime.
 // ============================================================
 
-const CACHE_VERSION = 'madrid-2026-07-31-1330';
+const CACHE_VERSION = 'madrid-2026-07-31-1600';
 
 // Fichiers mis en cache pour le hors-ligne
 const ASSETS = [
